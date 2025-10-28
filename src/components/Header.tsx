5c45b76ad1f3362981604dc28b17d758dac78437
AnthonySTZ <anthonycandy2309@gmail.com>
2025-10-28 16:06:44 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import logo from "../assets/images/logoBioInnovex.png"
 export default function Header() {
     return (
         <div className="flex justify-between items-center p-8 bg-white shadow">
-            <img src={logo} className="h-12 w-auto"/>
-            <div className="flex justify-evenly items-center gap-8 text-blue-500 font-semibold">
+            <a href="/"><img src={logo} className="h-12 w-auto"/></a>
+            <div className="text-lg flex justify-evenly items-center gap-8 text-blue-500 font-semibold">
                 <a href="/">About us</a>
                 <a href="#our-services">Our services</a>
                 <a href="#references">References</a>
