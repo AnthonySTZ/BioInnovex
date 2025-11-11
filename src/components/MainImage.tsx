e51a4c6da6ca47577146bf4bb18a7323d1152b6a
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-11 11:19:40 +0100

    main image for mobile

--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -2,10 +2,10 @@ import purpleStrings from "../assets/images/purple_strings.png"
 
 export default function MainImage() {
     return (
-        <div className="flex bg-cover bg-center py-32 mx-16"
+        <div className="flex bg-cover bg-center py-32 mx-4 md:mx-16"
         style= { { backgroundImage: `url(${purpleStrings})` } }>
             <div className="bg-blue-600/80 p-12 py-24 rounded-r-lg">
-                <h1 className="text-6xl text-white font-semibold leading-tight">
+                <h1 className="text-3xl lg:text-6xl text-white font-semibold leading-tight">
                     YOU’VE BUILT VALUE<br/>
                     WE HELP YOU UNLOCK<br/>
                     ITS FULL POTENTIAL
