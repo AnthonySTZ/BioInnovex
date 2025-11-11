30ba8e016240fbdc4d792579d7d8bbb3698807c9
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-11 11:34:00 +0100

    services

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -4,10 +4,10 @@ import ServiceItem from "./ServiceItem";
 
 export default function OurServices() {
     return (
-        <div id="our-services" className="mx-16">
-            <h1 className="text-6xl font-bold text-blue-600">OUR SERVICES:</h1>
-            <div className="p-8 grid grid-cols-3 gap-8">
-                <div className="col-start-3 row-span-3 bg-center bg-cover"
+        <div id="our-services" className="mx-4 lg:mx-16">
+            <h1 className="text-5xl lg:text-6xl font-bold text-blue-600">OUR SERVICES:</h1>
+            <div className="flex flex-col p-8 lg:grid lg:grid-cols-3 gap-8">
+                <div className="col-start-3 row-span-3 bg-center bg-cover hidden lg:block"
                 style={{ backgroundImage: `url(${microscope})`}}></div>
                 {
                    services.map((service, index) => (
