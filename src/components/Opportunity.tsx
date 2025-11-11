fe2930678608c35dad9f019f5a5401c49a720827
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-11 11:52:36 +0100

    network

--- src/components/Opportunity.tsx
+++ src/components/Opportunity.tsx
@@ -6,19 +6,19 @@ import Timeline from "./Timeline";
 export default function Opportunity() {
     return (
         <div className="mx-4 lg:mx-16 flex justify-between gap-16">
-            <div className="flex flex-col gap-16">
+            <div className="flex flex-col gap-8 md:gap-16">
                 <h1 className="text-4xl lg:text-6xl font-bold text-blue-600">FROM OPPORTUNITY TO REVENUE</h1>
                 <p className="text-blue-600 text-2xl md:text-3xl">
                     Our approach ensures a seamless transition from strategy to
                     execution, delivering measurable results on the ground.
                 </p>
                 <Timeline />
-                <h3 className="text-blue-600 text-4xl font-semibold">
+                <h3 className="text-blue-600 text-3xl md:text-4xl font-semibold">
                     AVERAGE TIME-TO-MARKET FOR<br/>
                     RARE DISEASES TREATMENTS<br/>
                     6–9 MONTHS
                 </h3>
-                <p className="text-blue-600 text-3xl">
+                <p className="text-blue-600 text-2xl md:text-3xl">
                     We provide a turnkey solution from feasibility study to launch, with
                     minimal internal resource allocation from our clients. Thanks to this
                     flexible “modular” model, companies can stop at any stage and keep
