80b754f8b456f71491443f55afd7d221dde42486
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-20 11:51:41 +0100

    feat(App): use backgroundImage for the entire website

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,32 @@ import Opportunity from './components/Opportunity'
 import OurServices from './components/OurServices'
 import ValueCreation from './components/ValueCreation'
 import purpleStringsBlur from "./assets/images/purple_strings_blur.png"
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [opacity, setOpacity] = useState(1);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const fadePoint = 300;
+        const newOpacity = Math.max(1 - window.scrollY / fadePoint, 0);
+        setOpacity(newOpacity);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
   return (
-    <main className='flex flex-col gap-4 lg:gap-8 overflow-y-scroll no-scrollbar'>
-      <div className="fixed bg-cover bg-center w-full h-screen" style={{ backgroundImage: `url(${purpleStringsBlur})` }}>
+    <main className='flex flex-col gap-4 lg:gap-8 overflow-y-scroll no-scrollbar bg-cover bg-center' style={{ backgroundImage: `url(${purpleStringsBlur})` }}>
+      <div className="fixed w-full h-screen" style={{ opacity }}>
         <div className='fixed top-0 left-0 right-0 z-40'>
         <Header />
         </div>
         <MainImage />
       </div>
-      <div className="mt-[100vh] md:pt-0 z-10 bg-gray-50">
+      <div className="mt-[100vh] md:pt-0 z-10">
         <NewMarkets />
         <OurServices />
         <Opportunity />
--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -1,24 +1,8 @@
-import { useEffect, useState } from "react";
-import purpleStrings from "../assets/images/purple_strings.png"
-
 export default function MainImage() {
-
-    const [opacity, setOpacity] = useState(1);
-
-    useEffect(() => {
-        const handleScroll = () => {
-        const fadePoint = 600;
-        const newOpacity = Math.max(1 - window.scrollY / fadePoint, 0);
-        setOpacity(newOpacity);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
 
     return (
         <div className="fixed bg-cover bg-center w-full h-screen">
-            <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-30" style={{ opacity }}>
+            <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-30">
                 <h1 className="relative text-5xl lg:text-6xl font-semibold text-center leading-tight text-blue-700">
                     YOU’VE BUILT VALUE<br/>
                     WE HELP YOU UNLOCK<br/>
